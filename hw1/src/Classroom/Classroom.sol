9f113488acae1274db282cb5d507245d21ec81bb
uuuu <weber0205@gmail.com>
2024-03-26 11:37:47 +0800

    Classroom first try

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -4,9 +4,15 @@ pragma solidity ^0.8.0;
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
     // Note: You can declare some state variable
+    uint256 code = 1000;
+    uint count = 0;
 
     function register() external returns (uint256) {
         // TODO: please add your implementaiton here
+        if (count != 0) code = 123;
+
+        count += 1;
+        return code; 
     }
 }
 
@@ -18,6 +24,12 @@ interface IClassroomV2 {
 contract StudentV2 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        if (IClassroomV2(msg.sender).isEnrolled() == false) {
+            return 1000;
+        } 
+        else {
+            return 123;
+        }
     }
 }
 
@@ -25,5 +37,11 @@ contract StudentV2 {
 contract StudentV3 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        if (gasleft() >= 7000) {
+            return 1000;
+        } 
+        else {
+            return 123;
+        }
     }
 }
